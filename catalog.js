d9dd9ad31655beb6bf05cf4db5839551d9043f0e
sqdyou <hanyourmd@gmail.com>
2017-09-14 00:28:06 -0600

    add progress bar

--- catalog.js
+++ catalog.js
@@ -37,6 +37,13 @@ function render_schedule(courses){
 
 function get_course_schedule_html(callback){
     $('#cceasier-loading-indicator').html('Easier CC Banner: Loading... <small>(first loading usually takes about 10s)</small>');
+    var bar = new ldBar('#cceasier-loading-indicator-progress');
+    var start_time = new Date().getTime();
+    var timer = setInterval(function(){
+        var t = (new Date().getTime() - start_time) / 1000;
+        var p = 100*(1-Math.pow(0.8, t));
+        bar.set(p);
+    }, 1000);
     var site_map_link = $("a:contains('SITE')")[0];
     $.get(site_map_link.getAttribute('href'), function(html){
         var re = /href="(\w+\.p_disp_dyn_sched)"/;
@@ -54,7 +61,11 @@ function get_course_schedule_html(callback){
                 var re = /action="(\/prod\/\w+\.p_disp_dyn_sched_list)"/;
                 var form_url = re.exec(html)[1];
                 var post_data = `term_in=${term1}&term_in=${term2}&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy&sel_levl=dummy&sel_sess=dummy&sel_instr=dummy&sel_type=dummy&sel_rtng=dummy&sel_ptrm=dummy&sel_attr=dummy&sel_subj=%25&sel_crse=&sel_title=&sel_ptrm=%25&sel_instr=%25&sel_attr=%25&sel_vcncy=%25&sel_type=%25&sel_camp=%25&sel_rtng=%25&sel_schd=%25&sel_insm=%25&begin_hh=00&begin_mi=00&begin_ap=x&end_hh=00&end_mi=00&end_ap=xe`;
-                $.post(form_url, post_data, callback);
+                $.post(form_url, post_data, function(html){
+                    clearInterval(timer);
+                    bar.set(100);
+                    callback(html);
+                });
             });
         });
     });
@@ -93,12 +104,17 @@ function parse_schedule_html(html){
 }
 
 function main(){
-    
-
     var header_col = $('.pldefault:contains(Catalog Entries)')[0];
-    var loadind_indicator = document.createElement('h3');
-    loadind_indicator.setAttribute('id', 'cceasier-loading-indicator');
-    header_col.appendChild(loadind_indicator);
+    var loading_indicator = document.createElement('h3');
+    loading_indicator.setAttribute('id', 'cceasier-loading-indicator');
+    header_col.appendChild(loading_indicator);
+
+    var loading_bar = document.createElement('div');
+    loading_bar.setAttribute('id', 'cceasier-loading-indicator-progress');
+    loading_bar.setAttribute('class', 'ldBar label-center');
+    loading_bar.setAttribute('data-value', '0');
+    loading_bar.setAttribute('data-preset', 'circle');
+    header_col.appendChild(loading_bar);
 
     try{
         if(new Date().getTime() - parseInt(localStorage.getItem('cceasier-timestamp')) < 3600000){
